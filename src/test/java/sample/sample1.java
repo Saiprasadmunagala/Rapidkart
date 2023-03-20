83142428d670da1397a7588a93f317a7039a6452
Saiprasadmunagala <103264670+Saiprasadmunagala@users.noreply.github.com>
2023-03-20 07:02:10 +0530

    Update sample1.java

--- src/test/java/sample/sample1.java
+++ src/test/java/sample/sample1.java
@@ -19,7 +19,8 @@ public static void main(String[]args) {
 	 driver.get("https://www.youtube.com/");
 	 driver.findElement(By.id("search-input")).sendKeys("HappyBirthday");
 	 driver.findElement(By.id("search-icon-legacy")).click();
-	 driver.findElement(By.xpath("//img[@src=\"https://i.ytimg.com/vi/_z-1fTlSDF0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBF_7ZJAXzp5qTfnhF8bkkVCKhh0w\"]")).click(); 
+	 driver.findElement(By.xpath("//img[@src=\"https://i.ytimg.com/vi/_z-1fTlSDF0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBF_7ZJAXzp5qTfnhF8bkkVCKhh0w\"]")).click();
+	System.out.println("Hi This changes was made in the Github");
 	}
 
 }
